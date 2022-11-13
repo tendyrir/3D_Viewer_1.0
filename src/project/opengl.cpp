2473f12f64573876e57bd052110b7aeb359edb68
Sandslash Dorla <sandslas@ga-t4.msk.21-school.ru>
2022-11-13 15:08:04 +0300

    draw cube

--- src/project/opengl.cpp
+++ src/project/opengl.cpp
@@ -2,31 +2,78 @@
 
 openGL::openGL(QWidget *parent)
     : QOpenGLWidget(parent) {
-
+    setGeometry(400, 200, 800,600);
 }
 
 void openGL::initializeGL() {
     initializeOpenGLFunctions();
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f); // черный цвет
     glEnable(GL_DEPTH_TEST); // включение буффера глубины
-    glEnable(GL_CULL_FACE); // отключает задние грани
-    glEnable(GL_LIGHT0);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_COLOR_MATERIAL);
-    initShaders();
+//    glEnable(GL_CULL_FACE); // отключает задние грани
+//    glEnable(GL_LIGHT0);
+//    glEnable(GL_LIGHTING);
+//    glEnable(GL_COLOR_MATERIAL);
 }
 void openGL::resizeGL(int width, int height) {
     glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+//    glOrtho(-1,1,-1,1,1,2);
+    glFrustum(-1,1,-1,1,1,3);
 }
+
+
 void openGL::paintGL() {
-     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//    qglClearColor(Qt::darkCyan);
-//    qglColor(Qt::red);
-//    glBegin(GL_LINE_LOOP);
-//        update();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glTranslatef(0,0,-2);
+    glRotatef(xRot, 1, 0, 0);
+    glRotatef(yRot, 0, 1, 0);
+    drawCube(0.5);
+}
+
+void openGL::drawCube(float a) {
+    float ver_cub[] = {
+         -a, -a, a,     a, -a, a,     a, a, a,     -a, a, a, // передняя
+         a, -a, -a,     -a, -a, -a,     -a, a, -a,     a, a, -a, // задняя
+         -a, -a, -a,     -a, -a, a,     -a, a, a,     -a, a, -a, // левая
+         a, -a, a,     a, -a, -a,     a, a, -a,     a, a, a, // правая
+         -a, -a, a,     a, -a, a,     a, -a, -a,     -a, -a, -a, // нижняя
+         -a, a, a,     a, a, a,     a, a, -a,     -a, a, -a, // верхняя
+    };
+    float color[] = {
+        1, 0, 0,     1, 0, 0,     1, 0, 0,     1, 0, 0,
+        0, 0, 1,     0, 0, 1,     0, 0, 1,     0, 0, 1,
+        1, 1, 0,     1, 1, 0,     1, 1, 0,     1, 1, 0,
+        0, 1, 1,     0, 1, 1,     0, 1, 1,     0, 1, 1,
+        1, 0, 1,     1, 0, 1,     1, 0, 1,     1, 0, 1,
+        1, 0.5, 0.5,     1, 0.5, 0.5,     1, 0.5, 0.5,     1, 0.5, 0.5,
+    };
+    glVertexPointer(3, GL_FLOAT, 0, &ver_cub);
+    glEnableClientState(GL_VERTEX_ARRAY);
+
+    glColorPointer(3, GL_FLOAT, 0, &color);
+    glEnableClientState(GL_COLOR_ARRAY);
+
+    glDrawArrays(GL_QUADS, 0, 24);
+
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
 
 }
 
-void openGL::initShaders() {
-//    m_program.addShaderFromSourceFile(QOpenGLShader::Vertex, "vshader")
+void openGL::mousePressEvent(QMouseEvent *mo) {
+    mPos = mo->pos();
+
 }
+
+void openGL::mouseMoveEvent(QMouseEvent *mo) {
+    xRot = 1 / M_PI * (mo->pos().y() - mPos.y());
+    yRot = 1 / M_PI * (mo->pos().x() - mPos.x());
+    update();
+}
+
+
+
--- src/project/opengl.h
+++ src/project/opengl.h
@@ -5,22 +5,28 @@
 #include <QtOpenGL>
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
-#include <QOpenGLShaderProgram>
-#include <QOpenGLTexture>
+//#include <gl/GL.h>
+//#include <gl/GLU.h>
+//#include <QOpenGLShaderProgram>
+//#include <QOpenGLTexture>
+//#include <QOpenGLBuffer>
 
 class openGL : public QOpenGLWidget, protected QOpenGLFunctions {
     Q_OBJECT
 public:
     explicit openGL(QWidget *parent = nullptr);
 protected:
-    void initializeGL();
-    void resizeGL(int w, int h);
-    void paintGL();
-    void initShaders();
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
 
 private:
-    QOpenGLShaderProgram m_program;
-    QOpenGLTexture m_texture;
+    float xRot, zRot, yRot;
+    QPoint mPos;
+    QTimer tmr;
+    void drawCube(float a);
+    void mousePressEvent(QMouseEvent*) override;
+    void mouseMoveEvent(QMouseEvent*) override;
 };
 
 #endif // OPENGL_H
